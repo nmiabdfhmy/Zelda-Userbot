3925b9182c6462dcc377db018889421d7ac63b46
Zelda Projects <56251778+nmiabdfhmy@users.noreply.github.com>
2022-09-03 06:48:55 +0700

    Update help.py

--- Zelda/modules/help.py
+++ Zelda/modules/help.py
@@ -1,5 +1,4 @@
-from prettytable import PrettyTable, NONE
-
+# from prettytable import PrettyTable, NONE
 from Zelda import CHANNEL
 from Zelda import CMD_HANDLER as cmd
 from Zelda import CMD_HELP, ICON_HELP, bot
@@ -23,25 +22,27 @@ async def help(event):
         user = await bot.get_me()
         string = ""
         
-        ac = PrettyTable()
-        ac.header = False
-        ac.align = "l"
-        ac.vertical_char = "│"
-        ac.hrules = NONE
-        for x in split_list(sorted(modules.keys()), 2):
-        	ac.add_row([x[0], x[1] if len(x) >= 2 else None])
+        # ac = PrettyTable()
+        # ac.header = False
+        # ac.align = "l"
+        # ac.vertical_char = "│"
+        # ac.hrules = NONE
+        # for x in split_list(sorted(modules.keys()), 2):
+        	# ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         
         for i in CMD_HELP:
-            string += "📌 `" + str(i) + "` "
+            string += "• `" + str(i) + "` "
 
         await edit_or_reply(
             event,
             f"**Daftar Perintah Untuk [ZELDA USERBOT](https://github.com/nmiabdfhmy/Zelda-Userbot) :**\n\n"
             f"**Jumlah : ** `{len(modules)}` Modules\n"
             f"**Owner : ** [Lord Zelda](https://t.me/UnrealZlda)\n\n"
-            f"```{ac}```"
+            f"{string}"
             f"\n\nJoin and Support @{CHANNEL}",
         )
         await event.reply(
             f"\n**Contoh Ketik** `{cmd}help ping` **Untuk Melihat Informasi Module**"
         )
+
+
